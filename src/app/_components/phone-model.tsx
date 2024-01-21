1f4923865f4a59f6b2cf5c42456a93329969005d
Hugo-Dussert <hugo.dussert@dalma.co>
2024-01-21 12:34:01 +0100

    fix: quickfix useGLTF typing

--- src/app/_components/phone-model.tsx
+++ src/app/_components/phone-model.tsx
@@ -1,14 +1,17 @@
 import { Html, useGLTF } from "@react-three/drei";
 import { useControls } from "leva";
+import type { GLTF } from "three-stdlib";
+
+const PHONE_MODEL_URL =
+  "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf";
 
 export default function PhoneModel() {
   const { distanceFactor } = useControls({ distanceFactor: 1.17 });
-  const { scene } = useGLTF(
-    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf",
-  );
+  const phoneGLTF = useGLTF(PHONE_MODEL_URL);
+
   return (
     <primitive
-      object={scene}
+      object={phoneGLTF.scene as unknown as GLTF} // TODO: Find a way to type useGLTF's return
       position-y={-1.2}
       position-x={-0.1}
       position-z={0}
@@ -26,3 +29,5 @@ export default function PhoneModel() {
     </primitive>
   );
 }
+
+useGLTF.preload(PHONE_MODEL_URL);
