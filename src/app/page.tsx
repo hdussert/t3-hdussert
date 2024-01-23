e7b3500aeaed16069742ceefa51ee65f95acc80b
hdussert <34503603+hdussert@users.noreply.github.com>
2024-01-23 03:59:28 +0000

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,58 +16,65 @@ export default async function Home() {
           <h1 className="group text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Hugo Dussert
           </h1>
-          <div className="h-96">
-            <Phone />
-          </div>
+          <Phone />
         </div>
 
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex  max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 transition hover:bg-white/20"
+          <CardLink 
             href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Mon blog →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            title="Mon blog"
+            description="Just the basics - Everything you need to know to set up your database and authentication."
+          />
+          <CardLink 
             href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Me contacter →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+            title="Me contacter"
+            description="Learn more about Create T3 App, the libraries it uses, and how to deploy it."
+          />
         </div>
-        <div className="flex flex-col items-center gap-2">
+        {/* <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
-        </div>
-        <CrudShowcase />
+        </div> */}
+        {/* <CrudShowcase /> */}
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
+type CardLinkProps = {
+  href: string;
+  title: string;
+  description: string;  
+}
 
+const CardLink = ({ href, title, description }: CardLinkProps) => {
   return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
+    <Link
+      className="flex  max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 transition hover:bg-white/20"
+      href={href}
+      target="_blank"
+    >
+      <h3 className="text-2xl font-bold">{title} →</h3>
+      <div className="text-lg">
+        {description}
+      </div>
+    </Link>
   );
 }
+
+// async function CrudShowcase() {
+//   const latestPost = await api.post.getLatest.query();
+
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
+
+//       <CreatePost />
+//     </div>
+//   );
+// }
