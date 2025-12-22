f7feadfb615601671483b7a35d5b4ce4e9d7b213
hdussert <hdussert@student.42.fr>
2025-12-23 00:27:24 +0100

    quick fix to make project clickable on mobile

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -4,18 +4,34 @@ type ProjectProps = ProjectData & {
   onClick?: () => void;
 };
 
-const Project = ({ title, description, onClick }: ProjectProps) => {
+const Project = ({ title, description, url, onClick }: ProjectProps) => {
   return (
-    <div
-      key={title}
-      className="hover:bg-background z-10 flex aspect-square max-w-56 cursor-pointer flex-col items-center justify-center rounded-lg border p-4 text-center transition-all hover:z-20 hover:scale-110"
-      onClick={onClick}
-    >
-      <h3 className="font-display text-lg">{title}</h3>
-      <p className="text-muted-foreground text-sm text-balance">
-        {description}
-      </p>
-    </div>
+    <>
+      {/* Mobile: Render <a> */}
+      {/* TODO : Change design */}
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:bg-background z-10 flex aspect-square max-w-56 cursor-pointer flex-col items-center justify-center rounded-lg border p-4 text-center transition-all hover:z-20 hover:scale-110 sm:hidden"
+      >
+        <h3 className="font-display text-lg">{title}</h3>
+        <p className="text-muted-foreground text-sm text-balance">
+          {description}
+        </p>
+      </a>
+
+      {/* Desktop: Render <div> */}
+      <div
+        className="hover:bg-background z-10 hidden aspect-square max-w-56 cursor-pointer flex-col items-center justify-center rounded-lg border p-4 text-center transition-all hover:z-20 hover:scale-110 sm:flex"
+        onClick={onClick}
+      >
+        <h3 className="font-display text-lg">{title}</h3>
+        <p className="text-muted-foreground text-sm text-balance">
+          {description}
+        </p>
+      </div>
+    </>
   );
 };
 
