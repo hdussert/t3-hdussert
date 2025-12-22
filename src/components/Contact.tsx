2c35fdba722cfb02f2bc775cb5a7b97a323f5756
hdussert <hdussert@student.42.fr>
2025-12-22 22:35:13 +0100

    prevent contact form reset on validation error

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,105 +1,124 @@
 import { useActionState } from "react";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
-import { sendEmail, SendEmailFormData } from "~/actions/send-email";
+import { EmailData, sendEmail, SendEmailFormData } from "~/actions/send-email";
 import { ActionResponse } from "~/actions/utils";
 import Section from "~/components/Section";
 import SectionTitle from "~/components/SectionTitle";
 import { Button } from "~/components/ui/button";
 import { Field, FieldError, FieldLabel } from "~/components/ui/field";
 import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
 
-const initialFormState: ActionResponse = {
+type FormState = ActionResponse & { data: EmailData };
+
+const initialFormState: FormState = {
   success: false,
   message: "",
+  data: {
+    source: "",
+    subject: "",
+    body: "",
+  },
 };
 
 const Contact = () => {
   const { executeRecaptcha } = useGoogleReCaptcha();
-  const [state, formAction, isPending] = useActionState<
-    ActionResponse,
-    FormData
-  >(async (_: ActionResponse, formData: FormData) => {
-    try {
-      // ReCAPTCHA verification
-      if (!executeRecaptcha) {
-        return {
-          success: false,
-          message: "ReCAPTCHA not yet available",
-        };
-      }
-
-      const recaptchaToken = await executeRecaptcha("contact_form");
-
-      const data: SendEmailFormData = {
+  const [state, formAction, isPending] = useActionState<FormState, FormData>(
+    async (_: FormState, formData: FormData) => {
+      const data: EmailData = {
         source: formData.get("source") as string,
         subject: formData.get("subject") as string,
         body: formData.get("body") as string,
-        recaptchaToken,
       };
-      const result = await sendEmail(data);
 
-      if (result.success) {
-        // toast.success(result.message);
+      try {
+        // ReCAPTCHA verification
+        if (!executeRecaptcha) {
+          return {
+            success: false,
+            message: "ReCAPTCHA not yet available",
+            data,
+          };
+        }
+
+        const recaptchaToken = await executeRecaptcha("contact_form");
+
+        const result = await sendEmail({
+          ...data,
+          recaptchaToken,
+        } as SendEmailFormData);
+
+        if (result.success) {
+          // toast.success(result.message);
+        }
+
+        return {
+          ...result,
+          data: initialFormState.data, // Reset form data on success
+        };
+      } catch (error) {
+        return {
+          success: false,
+          message: "An unexpected error occurred.",
+          data,
+        };
       }
-
-      return result;
-    } catch (error) {
-      return {
-        success: false,
-        message: "An unexpected error occurred.",
-      };
-    }
-  }, initialFormState);
+    },
+    initialFormState,
+  );
 
   return (
     <Section className="h-screen">
-      <SectionTitle>Me contacter</SectionTitle>
+      <SectionTitle className="text-center">Me contacter</SectionTitle>
       <form action={formAction} className="flex flex-col gap-4">
-        <Field>
-          <FieldLabel htmlFor="source">Votre email</FieldLabel>
-          <Input
-            id="source"
-            name="source"
-            type="email"
-            placeholder="mail@exemple.com"
-            required
-            disabled={isPending}
-          />
-          <FieldError>
-            {state.errors?.source && state.errors.source[0]}
-          </FieldError>
-        </Field>
+        <div className="flex gap-4">
+          <Field>
+            <FieldLabel htmlFor="source">Votre email</FieldLabel>
+            <Input
+              id="source"
+              name="source"
+              type="email"
+              placeholder="mail@exemple.com"
+              required
+              disabled={isPending}
+              defaultValue={state.data.source}
+            />
+            <FieldError>
+              {state.errors?.source && state.errors.source[0]}
+            </FieldError>
+          </Field>
 
-        <Field>
-          <FieldLabel htmlFor="subject">Sujet</FieldLabel>
-          <Input
-            id="subject"
-            name="subject"
-            placeholder="Sujet..."
-            required
-            disabled={isPending}
-          />
-          <FieldError>
-            {state.errors?.subject && state.errors.subject[0]}
-          </FieldError>
-        </Field>
+          <Field>
+            <FieldLabel htmlFor="subject">Sujet</FieldLabel>
+            <Input
+              id="subject"
+              name="subject"
+              placeholder="Sujet..."
+              required
+              disabled={isPending}
+              defaultValue={state.data.subject}
+            />
+            <FieldError>
+              {state.errors?.subject && state.errors.subject[0]}
+            </FieldError>
+          </Field>
+        </div>
 
         <Field>
           <FieldLabel htmlFor="body">Message</FieldLabel>
-          <textarea
+          <Textarea
             id="body"
             name="body"
             placeholder="Votre message..."
             required
-            className="w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none disabled:opacity-50"
-            rows={5}
             disabled={isPending}
-          ></textarea>
+            defaultValue={state.data.body}
+          />
           <FieldError>{state.errors?.body && state.errors.body[0]}</FieldError>
-          <Button type="submit" disabled={isPending} className="mt-4">
-            {isPending ? "Envoi en cours..." : "Envoyer"}
-          </Button>
         </Field>
+        <Button type="submit" disabled={isPending} className="mt-4">
+          {isPending ? "Envoi en cours..." : "Envoyer"}
+        </Button>
       </form>
     </Section>
   );
